52d4066605b12855577b3b32d079875441441c3b
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-04-05 10:14:02 -0300

    refactor: validate request

--- Dtos/User/CreateUserReq.cs
+++ Dtos/User/CreateUserReq.cs
@@ -4,15 +4,17 @@ namespace cookbook_api.Dtos.User;
 
 public class CreateUserReq
 {
-    [Required]
-    [StringLength(50)]
+    [Required(ErrorMessage = "Nome é obrigatório")]
+    [StringLength(
+    50, MinimumLength = 3, ErrorMessage = "Nome precisa ter no mínimo {2} e no máximo {1} caracteres")]
     public string Name { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Email é obrigatório")]
     [StringLength(50)]
     public string Email { get; set; }
     
-    [Required]
-    [StringLength(60, MinimumLength = 6)]
+    [Required(ErrorMessage = "Senha é obrigatório")]
+    [StringLength(
+    60, MinimumLength = 6, ErrorMessage = "Senha precisa ter entre {2} e {1} caracteres")]
     public string Passord { get; set; }
 }
