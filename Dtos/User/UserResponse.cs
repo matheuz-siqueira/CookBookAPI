f287f357e64bd56e5a65ff04820254f1caff7368
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-04-04 22:46:48 -0300

    refactor: change response user dto

--- Dtos/User/UserResponse.cs
+++ Dtos/User/UserResponse.cs
@@ -2,7 +2,5 @@ namespace cookbook_api.Dtos.User;
 
 public class UserResponse
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public string Token { get; set; }
 }
