58ce9eeafa484068d2d5575235526beeea44de67
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-04-06 23:49:45 -0300

    feat: add recipe repository

--- Repositories/RecipeRepository.cs
+++ Repositories/RecipeRepository.cs
@@ -6,10 +6,9 @@ namespace cookbook_api.Repositories;
 public class RecipeRepository
 {
     private readonly Context _context;
+
     public RecipeRepository([FromServices] Context context)
     {
         _context = context;
     }
-
-    
 }
